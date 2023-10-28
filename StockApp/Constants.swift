e1be9bd4d1c6b0393928be800fe66c18fd5086ec
Zijie Gan <liangjie-gongten@foxmail.com>
2023-10-28 04:16:02 -0700

    Done

---
+++ StockApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  StockApp
+//
+//  Created by Teemo on 10/21/23.
+//
+
+import Foundation
+
+
+let apiKey = "1337c06f2d80beb469ecc68de59eafec"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
+
